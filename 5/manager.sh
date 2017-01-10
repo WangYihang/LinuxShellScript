eae934265a695afbf8adfc9beed028f3dd70970b
WangYihang <wangyihanger@gmail.com>
2017-01-10 15:26:02 +0800

    脚本管理执行工具

--- 5/manager.sh
+++ 5/manager.sh
@@ -9,10 +9,16 @@ for filename in `ls -I $0 ./scripts/`
 	do
 		echo -e "$[${i}+1]" '==>' "$filename"
 		i=$((i+1))
-		files[${i}]=${filename}
+		files[${i}]=${filename} # 使用关联数组建立用户选择的数字和脚本名称的对应关系
 		numbers="${numbers}|${i}"
 	done
 numbers="("${numbers}"|)"
-echo "请选择需要执行的脚本 : $numbers ?l"
-
-
+echo "请选择需要执行的脚本 : $numbers ?"
+# 根据用户选择进行选择脚本执行
+read userinput
+for index in ${!files[@]};do
+	if [ "$userinput" == $index ];then
+		/bin/bash ./scripts/${files[$userinput]}		
+		break
+	fi
+done
