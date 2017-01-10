58d38ee83b4a6fb582f181b32886811661983106
WangYihang <wangyihanger@gmail.com>
2017-01-10 09:24:53 +0800

    循环遍历指定文件夹下的脚本文件 , 让用户自行选择编号并执行

---
+++ 5/manager.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+declare -A files # 声明一个关联数组
+
+i=0
+numbers=""
+
+for filename in `ls -I $0 ./scripts/`
+	do
+		echo -e "$[${i}+1]" '==>' "$filename"
+		i=$((i+1))
+		files[${i}]=${filename}
+		numbers="${numbers}|${i}"
+	done
+numbers="("${numbers}"|)"
+echo "请选择需要执行的脚本 : $numbers ?"
