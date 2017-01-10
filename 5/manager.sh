9ea617b7663d3e20ef34c2b30808b089e24b225d
WangYihang <wangyihanger@gmail.com>
2017-01-10 17:25:33 +0800

    脚本管理工具

--- 5/manager.sh
+++ 5/manager.sh
@@ -7,10 +7,10 @@ numbers=""
 
 for filename in `ls -I $0 ./scripts/`
 	do
-		echo -e "$[${i}+1]" '==>' "$filename"
-		i=$((i+1))
+		echo -e "${i}" '==>' "$filename"
 		files[${i}]=${filename} # 使用关联数组建立用户选择的数字和脚本名称的对应关系
 		numbers="${numbers}|${i}"
+		i=$((i+1))
 	done
 numbers="("${numbers}"|)"
 echo "请选择需要执行的脚本 : $numbers ?"
