281046c2e9899170db94bb98062f30fbc963a153
WangYihang <wangyihanger@gmail.com>
2017-01-10 13:53:52 +0800

    字符串比较时的良好编码习惯

---
+++ 9/while.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# 应用场景 : 密码输入
+
+password="pa55w0rd"
+read -p "Password : " userinput
+
+while [ "${password}" != "$userinput" ] # 第一种 while 的语法
+	do
+		echo "Login failed!"
+		read -p "Password : " userinput
+	done
+echo "Login ok!"
+
