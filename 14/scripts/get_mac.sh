6875c8a78525f4d77b719994df317ec57c243ed4
WangYihang <wangyihanger@gmail.com>
2017-01-12 03:36:38 +0800

    获取网络信息(Mac/IP)

--- 14/scripts/get_mac.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ifconfig | grep -E "[[:blank:]][0123456789ABCDEF][0123456789abcdef]\:" | awk '{print $1" ==> "$5}'
--- 14/scripts/get_network_info.sh
+++ 14/scripts/get_network_info.sh
@@ -4,11 +4,14 @@ mac_all=`ifconfig | grep -v "lo" | grep -E -v "^[[:blank:]]" | tr -s "\n" | awk 
 for mac in $mac_all
 do
 	echo "网卡 : $mac"
+	mac_addr=`ifconfig $mac | grep -E "[[:blank:]][0123456789ABCDEF][0123456789abcdef]\:" | awk '{print $5}'`
 	ip_all=`ifconfig $mac | grep -E "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" | tr -s " " | awk '{print $2 " " $3 " " $4}' | tr -d 'A-Z' | tr -d 'a-z' | tr -d ':'`
 	ip_addr=`echo $ip_all | awk '{print $1}'`
 	ip_bcast=`echo $ip_all | awk '{print $2}'`
 	ip_mask=`echo $ip_all | awk '{print $3}'`
+	echo "    Mac 地址 : "$mac_addr
 	echo "    IP  地址 : "$ip_addr
 	echo "    广播地址 : "$ip_bcast
 	echo "    子网掩码 : "$ip_mask
+	echo "-----------------------------"
 done
