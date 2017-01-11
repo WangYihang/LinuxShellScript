2ff5c413bbcb5a9a178293ce73a5947caef3bce7
WangYihang <wangyihanger@gmail.com>
2017-01-12 03:30:24 +0800

    获取IP信息

---
+++ 14/scripts/get_ip_v4_detail.sh
@@ -0,0 +1,14 @@
+#!/bin/bah
+
+mac_all=`ifconfig | grep -v "lo" | grep -E -v "^[[:blank:]]" | tr -s "\n" | awk '{print $1}'`
+for mac in $mac_all
+do
+	echo "网卡 : $mac"
+	ip_all=`ifconfig $mac | grep -E "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" | tr -s " " | awk '{print $2 " " $3 " " $4}' | tr -d 'A-Z' | tr -d 'a-z' | tr -d ':'`
+	ip_addr=`echo $ip_all | awk '{print $1}'`
+	ip_bcast=`echo $ip_all | awk '{print $2}'`
+	ip_mask=`echo $ip_all | awk '{print $3}'`
+	echo "    IP  地址 : "$ip_addr
+	echo "    广播地址 : "$ip_bcast
+	echo "    子网掩码 : "$ip_mask
+done
--- 14/scripts/get_system_info.sh
+++ 14/scripts/get_system_info.sh
@@ -3,3 +3,4 @@
 echo "内核版本 : " `uname -r`
 echo "发行版 : " `lsb_release -d`
 echo "主机名 : " `hostname`
+echo "用户名 : " `whoami`
