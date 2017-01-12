7c747102cd623ac8bc413ea44ff8d8f45c1fb1fa
WangYihang <wangyihanger@gmail.com>
2017-01-12 10:37:19 +0800

    修复网卡名称过长不能正确获取到Mac的BUG

--- 14/scripts/get_network_info.sh
+++ 14/scripts/get_network_info.sh
@@ -4,7 +4,7 @@ mac_all=`ifconfig | grep -v "lo" | grep -E -v "^[[:blank:]]" | tr -s "\n" | awk 
 for mac in $mac_all
 do
 	echo "网卡 : $mac"
-	mac_addr=`ifconfig $mac | grep -E "[[:blank:]][0123456789ABCDEF][0123456789abcdef]\:" | awk '{print $5}'`
+	mac_addr=`ifconfig $mac | head -n 1 | awk '{print $5}'`
 	ip_all=`ifconfig $mac | grep -E "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" | tr -s " " | awk '{print $2 " " $3 " " $4}' | tr -d 'A-Z' | tr -d 'a-z' | tr -d ':'`
 	ip_addr=`echo $ip_all | awk '{print $1}'`
 	ip_bcast=`echo $ip_all | awk '{print $2}'`
