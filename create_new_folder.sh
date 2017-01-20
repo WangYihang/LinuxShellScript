a91714551ae18b50560aebe3c7244ba0d7f2757a
WangYihang <wangyihanger@gmail.com>
2017-01-20 08:51:53 +0800

    自动生成递增文件夹脚本

---
+++ create_new_folder.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+get_max_number(){
+	return `ls | sort -n | tail -n 1`
+}
+
+get_max_number
+max_number=$?
+
+echo "当前最大文件夹序号为 : $max_number"
+((new_folder_name=$max_number+1))
+echo "新文件夹名 : " $new_folder_name
+mkdir $new_folder_name
+get_max_number
+max_number=$?
+echo "新文件夹创建成功 , 当前最大序号为 : " $max_number
