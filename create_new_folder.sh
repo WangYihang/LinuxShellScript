b6a850d0c6d8f629cccf4f94a8e5c68fad815939
WangYihang <wangyihanger@gmail.com>
2017-02-08 06:29:14 +0800

    更新 create_new_folder.sh

--- create_new_folder.sh
+++ create_new_folder.sh
@@ -14,3 +14,9 @@ mkdir $new_folder_name
 get_max_number
 max_number=$?
 echo "新文件夹创建成功 , 当前最大序号为 : " $max_number
+cd $max_number
+touch ./README.md
+echo '目标' >> ./README.md
+echo '```' >> ./README.md
+echo '' >> ./README.md
+echo '```' >> ./README.md
